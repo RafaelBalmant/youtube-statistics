96f6ccc94f107e10608b16593f263f6323e17474
rafaelbalmant <balmantrafael@gmail.com>
2020-02-12 22:27:12 -0200

    feat: started develop header and created style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import Routes from './routes/Router';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import Header from "./components/UI/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'
 
 function App() {
   return (
     <>
+      <Header/>
       <Router>
         <Routes/>
       </Router>
